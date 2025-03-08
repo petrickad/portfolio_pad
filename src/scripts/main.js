3f2fde670adf8e7432ea4dac4340689f6ba31c17
petrickad <petrick.duarte99@gmail.com>
2025-03-08 16:07:02 -0300

    add projects/skills

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 AOS.init();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.nav-item');
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            items.forEach(el => el.classList.remove('nav-item--is-active'));
+            item.classList.add('nav-item--is-active');
+        });
+    });
+});
